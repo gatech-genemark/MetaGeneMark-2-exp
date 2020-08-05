d7926344d239461664c3248da26f2bbd5e1430f7
Karl Gemayel <karl@gatech.edu>
2020-08-05 10:33:34 -0300

    Visualize for large complete genomes

--- code/python/lib/mg_viz/stats_large.py
+++ code/python/lib/mg_viz/stats_large.py
@@ -21,6 +21,42 @@ def case_insensitive_match(df, col, value):
     return df[col].apply(lambda x: x.lower()) == value.lower()
 
 
+def plot_gc_stats_side_by_side(env, df_tidy, columns, tool_order, reference, **kwargs):
+    col_to_ylim = get_value(kwargs, "col_to_ylim", dict())
+
+    fig, axes = plt.subplots(1, len(columns), sharex="all", figsize=(8 * len(columns), 6))
+    reg_kws = {"lowess": True, "scatter_kws": {"s": 2}}
+
+    ax = None
+    for ax, col in zip(axes, columns):
+        for t in tool_order:
+            if t.lower() == reference.lower():
+                continue
+            df_curr = df_tidy[case_insensitive_match(df_tidy, "Tool", t)]
+
+            seaborn.regplot(
+                df_curr["Genome GC"], df_curr[col], label=t, color=CM.get_map("tools")[t.lower()],
+                **reg_kws, ax=ax
+            )
+
+            if col in col_to_ylim:
+                ax.set_ylim(*col_to_ylim[col])
+
+    if ax is not None:
+        fig.subplots_adjust(bottom=0.3)
+
+        handles, labels = ax.get_legend_handles_labels()
+        for lh in handles:
+            lh.set_alpha(1)
+            lh.set_sizes([8] * (len(tool_order)))
+        leg = fig.legend(handles, labels, bbox_to_anchor=(0.5, 0.2), loc='upper center',
+                         ncol=len(tool_order), bbox_transform=fig.transFigure)
+
+        fig.savefig(next_name(env["pd-work"]), bbox_inches="tight")
+
+    plt.show()
+
+
 def reorder_pivot_by_tool(df_pivoted, tool_order):
     # type: (pd.DataFrame, List[str]) -> pd.DataFrame
     return df_pivoted.reorder_levels([1, 0], 1)[
@@ -33,11 +69,9 @@ def viz_stats_large_3p_sn_sp(env, df_tidy, reference, **kwargs):
     # type: (Environment, pd.DataFrame, str, Dict[str, Any]) -> None
     tool_order = get_value(kwargs, "tool_order", sorted(df_tidy["Tool"].unique()))
 
-    reorder_pivot_by_tool(
-        df_tidy.pivot(index="Genome", columns="Tool", values=["Sensitivity", "Specificity"]),
-        tool_order
-    ).to_csv(
-        next_name(env["pd-work"], ext="csv")
+    plot_gc_stats_side_by_side(
+        env, df_tidy, ["Number of Found", "Sensitivity"], tool_order, reference,
+        col_to_ylim={"Specificity": (0, 1), "Sensitivity": (0, 1)}
     )
 
 
@@ -45,59 +79,21 @@ def stats_large_3p_predictions_vs_found(env, df_tidy, reference, **kwargs):
     # type: (Environment, pd.DataFrame, str, Dict[str, Any]) -> None
     tool_order = get_value(kwargs, "tool_order", sorted(df_tidy["Tool"].unique()))
 
-    reorder_pivot_by_tool(
-        df_tidy.pivot(index="Genome", columns="Tool", values=["Number of Predictions", "Number of Found"]),
-        tool_order
-    ).to_csv(
-        next_name(env["pd-work"], ext="csv")
+    plot_gc_stats_side_by_side(
+        env, df_tidy, ["Number of Predictions", "Number of Found", "Specificity"], tool_order, reference,
+        col_to_ylim={"Specificity": (0, 1), "Sensitivity": (0, 1)}
     )
 
 
-def viz_stats_large_5p_error_vs_found(env, df_tidy, reference, **kwargs):
+def viz_stats_large_5p_error_vs_sensitivity(env, df_tidy, reference, **kwargs):
     # type: (Environment, pd.DataFrame, str, Dict[str, Any]) -> None
     tool_order = get_value(kwargs, "tool_order", sorted(df_tidy["Tool"].unique()))
 
-    # plot against GC
-    fig, axes = plt.subplots(1, 2, sharex="all", figsize=(16, 6))
-    reg_kws = {"lowess": True, "scatter_kws": {"s": 2}}
-
-    ax = axes[0]
-    df_tidy["Error Rate"] = df_tidy["Number of Error"] / df_tidy["Number of Found"]     # FIXME: compute before
-    col = "Error Rate"
-    for t in tool_order:
-        if t.lower() == reference.lower():
-            continue
-        df_curr = df_tidy[case_insensitive_match(df_tidy, "Tool", t)]
-
-        seaborn.regplot(
-            df_curr["Genome GC"], df_curr[col], label=t, color=CM.get_map("tools")[t.lower()],
-            **reg_kws, ax=ax
-        )
-
-    ax = axes[1]
-    col = "Sensitivity"
-    for t in tool_order:
-        df_curr = df_tidy[case_insensitive_match(df_tidy, "Tool", t)]
-
-        if t.lower() == reference.lower():
-            continue
-        else:
-            seaborn.regplot(
-                df_curr["Genome GC"], df_curr[col], label=t, color=CM.get_map("tools")[t.lower()],
-                **reg_kws, ax=ax
-            )
-
-    handles, labels = ax.get_legend_handles_labels()
-    for lh in handles:
-        lh.set_alpha(1)
-        lh.set_sizes([8] * (len(tool_order)-1))
-
-    leg = fig.legend(handles, labels, bbox_to_anchor=(0.5, -0.05), loc='lower center',
-                     ncol=len(tool_order)-1)
-    fig.tight_layout()
-    fig.savefig(next_name(env["pd-work"]), bbox_extra_artists=(leg,), bbox_inches='tight')
-
-    plt.show()
+    df_tidy["Error Rate"] = df_tidy["Number of Error"] / df_tidy["Number of Found"]  # FIXME: compute before
+    plot_gc_stats_side_by_side(
+        env, df_tidy, ["Error Rate", "Sensitivity"], tool_order, reference,
+        col_to_ylim={"Specificity": (0, 1), "Sensitivity": (0, 1)}
+    )
 
 
 def viz_stats_large_3p(env, df_per_gene, tools, list_ref):
@@ -114,4 +110,4 @@ def viz_stats_large_5p(env, df_per_gene, tools, list_ref):
     reference, df_tidy = _helper_join_reference_and_tidy_data(env, df_per_gene, tools, list_ref)
 
     # Number of found vs number of 5' error
-    viz_stats_large_5p_error_vs_found(env, df_tidy, reference, tool_order=tools)
+    viz_stats_large_5p_error_vs_sensitivity(env, df_tidy, reference, tool_order=tools)
