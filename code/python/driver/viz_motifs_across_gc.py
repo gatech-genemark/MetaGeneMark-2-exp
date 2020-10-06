c877b2b77ef9082bee6c08be73bcd10bafa68a5e
Karl Gemayel <karl@gatech.edu>
2020-10-06 11:31:19 -0400

    Many changes for paper submission (figures)

--- code/python/driver/viz_motifs_across_gc.py
+++ code/python/driver/viz_motifs_across_gc.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 
 # noinspection All
-from mpl_toolkits.axes_grid1 import AxesGrid
+from mpl_toolkits.axes_grid1 import AxesGrid, make_axes_locatable
 
 import pathmagic
 
@@ -163,18 +163,30 @@ def visualize_matrix_column(env, df, col):
         #
         # plt.show()
         #
+        fig, ax = plt.subplots(figsize=(5.66,6))
+        df_tmp = pd.DataFrame({
+            "x1": embedding[:, 0], "x2": embedding[:, 1]
+        })
+        ax.scatter(df_tmp["x1"], df_tmp["x2"], s=2, color="black")
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        plt.tight_layout()
+        fig.savefig(next_name(env["pd-work"]), box_inches='tight')
+
+        plt.show()
+        
 
         # with cbar
         legend_size = 20
         # figsize = set_size("thesis", subplots=(2,3))
         fig = plt.figure(figsize=(17, 6)) #subplots(1, 2, figsize=(12, 6))
         # fig = plt.figure(figsize=figsize)  # subplots(1, 2, figsize=(12, 6))
-
+        # fig.subplots_adjust(bottom=0.1)
         grid = AxesGrid(fig, 111,
                         nrows_ncols=(1, 3),
                         axes_pad=0.05,
-                        cbar_mode='single',
-                        cbar_location='right',
+                        cbar_mode='each',
+                        cbar_location='bottom',
                         # cbar_pad=0.1
                         )
 
@@ -187,7 +199,10 @@ def visualize_matrix_column(env, df, col):
         for i, g in enumerate(sorted(df_tmp["Type"].unique())):
             df_subset = df_tmp[df_tmp["Type"] == g]
             axes[0].scatter(df_subset["x1"], df_subset["x2"], label=g, s=2, zorder=2-i)
-        leg = axes[0].legend(fontsize=fontsize)
+        leg = axes[0].legend(loc="upper center", ncol=3, fontsize=fontsize,
+                             bbox_to_anchor=(0.5, 0), frameon=False)
+
+        leg1 = leg
         for lh in leg.legendHandles:
             lh.set_sizes([legend_size] * len(df_tmp["Type"].unique()))
 
@@ -198,16 +213,24 @@ def visualize_matrix_column(env, df, col):
         for g in sorted(df_tmp["Group"].unique()):
             df_subset = df_tmp[df_tmp["Group"] == g]
             axes[1].scatter(df_subset["x1"], df_subset["x2"], label=g, s=2)
-        leg = axes[1].legend(ncol=2, fontsize=fontsize)
+        leg = axes[1].legend(loc="upper center", ncol=3, fontsize=fontsize,
+                             bbox_to_anchor=(0.5, 0), frameon=False)
+        leg2 = leg
         for lh in leg.legendHandles:
             lh.set_sizes([legend_size] * len(df_tmp["Group"].unique()))
 
         mappable = create_mappable_for_colorbar(gc, "viridis")
 
         axes[2].scatter(embedding[:, 0], embedding[:, 1], c=gc, s=2)
-        cbar = axes[2].cax.colorbar(mappable, fontsize=fontsize)
-        # cbar = grid.cbar_axes[0].colorbar(mappable)
 
+        # divider = make_axes_locatable(axes[2])
+        # cax = divider.append_axes("bottom", size="5%", pad=0.05)
+        # cbar = axes[2].colorbar(mappable, cax=cax)
+        # cbar = grid.cbar_axes[0].colorbar(mappable)
+        cbar = axes[2].cax.colorbar(mappable)
+        axes[2].cax.set_ylabel('Genome GC')
+        axes[0].cax.remove()
+        axes[1].cax.remove()
 
         # cleanup
         axes[0].get_xaxis().set_visible(False)
@@ -216,8 +239,13 @@ def visualize_matrix_column(env, df, col):
         axes[1].get_yaxis().set_visible(False)
         axes[2].get_xaxis().set_visible(False)
         axes[2].get_yaxis().set_visible(False)
-        plt.tight_layout()
-        plt.savefig(next_name(env["pd-work"]))
+
+        axes[0].set_title("1) Genome Type")
+        axes[1].set_title("2) GeneMarkS-2 Group")
+        axes[2].set_title("3) Genome GC content")
+        # plt.tight_layout()
+        # plt.savefig(next_name(env["pd-work"]))
+        fig.savefig(next_name(env["pd-work"]), bbox_extra_artists=(leg1, leg2, ), bbox_inches='tight')
         plt.show()
 
     # fig, ax = plt.subplots()
@@ -231,6 +259,7 @@ def main(env, args):
     logger.debug(f"Removing genetic code 4: {(df['Genetic Code'] == 4).sum()}")
     df = df[df["Genetic Code"] != 4]
     df = df.convert_dtypes().copy()
+    # df = df.sample(1000)
 
     df["REMOVE"] = (df["Type"] == "Archaea") & (df["GENOME_TYPE"].isin({"B", "C", "X"}))
     df = df[~df["REMOVE"]].copy()
