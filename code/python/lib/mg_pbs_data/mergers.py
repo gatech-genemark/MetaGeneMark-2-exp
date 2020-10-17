86ede93cb2e36b11e5c855c6721ed93d16362503
Karl Gemayel <karl@gatech.edu>
2020-10-17 09:40:43 -0400

    FIx appending to stats

--- code/python/lib/mg_pbs_data/mergers.py
+++ code/python/lib/mg_pbs_data/mergers.py
@@ -1,5 +1,7 @@
 import itertools
 import logging
+import os
+
 import pandas as pd
 from typing import *
 
@@ -20,19 +22,27 @@ def merge_dataframes(list_df):
     # type: (List[pd.DataFrame]) -> pd.DataFrame
     return pd.concat(list_df, ignore_index=True, sort=False)
 
+def file_not_empty(pf):
+    return os.path.getsize(pf) > 0
+
 def merge_dataframes_to_file(dfs, **kwargs):
     # type: (Iterable[pd.DataFrame], Dict[str, Any]) -> None
     pf_output = get_value(kwargs, "pf_output", required=True)
     append = get_value(kwargs, "append", default=False)
 
+    counter = 0     # counter == 0 means write header
     if not append:
         remove_p(pf_output)
-    counter = 0
+    else:
+        # disable header if exists
+        if os.path.isfile(pf_output) and file_not_empty(pf_output):
+            counter = 1
+
     header = None
     for df in dfs:
         df.sort_index(axis=1, inplace=True)
         #print(list(df.columns.values))
-        if header == None:
+        if header is None:
             header = list(df.columns.values)
         else:
 
